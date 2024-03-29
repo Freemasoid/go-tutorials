f2a19c89f868944500ad7d4eee57250448502426
Freemasoid <freemasoid@icloud.com>
2024-03-29 12:33:22 +0100

    error handling; file write

--- profit_calc/profit_calc.go
+++ profit_calc/profit_calc.go
@@ -1,27 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err1 := getUserInput("Revenue: ")
+	expenses, err2 := getUserInput("Expenses: ")
+	taxRate, err3 := getUserInput("Tax Rate: ")
+
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println(err1)
+		return
+	}
 
 	earningsBeforeTax, earningsAfterTax, ratio := calculator(revenue, expenses, taxRate)
 
 	fmt.Printf("Earnings Before Tax: %.1f\n", earningsBeforeTax)
 	fmt.Printf("Earnings After Tax: %.1f\n", earningsAfterTax)
 	fmt.Printf("Profit ratio: %.3f\n", ratio)
+	storeResults(earningsBeforeTax, earningsAfterTax, ratio)
 }
 
-func getUserInput(label string) float64 {
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func getUserInput(label string) (float64, error) {
 	var userInput float64
 	fmt.Print(label)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be greater than zero")
+	}
+
+	return userInput, nil
 }
 
 func calculator(revenue, expenses, taxRate float64) (float64, float64, float64) {
